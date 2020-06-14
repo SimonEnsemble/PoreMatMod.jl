8057806102596b6dff6526a8e481b76ca6762ed3
eahenle <eahenle@gmail.com>
2020-06-14 15:25:28 -0700

    Update moiety.jl

--- src/moiety.jl
+++ src/moiety.jl
@@ -1,6 +1,23 @@
+# moiety.jl
+# Adrian Henle, Cory Simon 2020
+
 using PorousMaterials
+
+## TODO
+"""
+    # add path_to_moieties to PorousMaterials
+    # moiety() tests:
+        # generate vtk
+        # generate manual graph representation
+        # verify that the two are identical
+"""
+## #
+
+
+"""
+    Generates a moiety (Crystal) from an .xyz file found in path_to_moieties
+"""
 function moiety(name::String)
-    # Generates a moiety (Crystal) from an .xyz file found in path_to_moieties
 
     box = unit_cube()
     fx = Frac(read_xyz("moieties/$(name).xyz"), box)
