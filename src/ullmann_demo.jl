8834683d6113b1903dd5f7021e110f33b1f4d75d
eahenle <eahenle@gmail.com>
2020-06-14 15:16:35 -0700

    Update ullmann_demo.jl

--- src/ullmann_demo.jl
+++ src/ullmann_demo.jl
@@ -1,8 +1,9 @@
+#!/usr/bin/env julia
 # ullmann_demo.jl
 # Adrian Henle, 2020
 # Demonstrates the function of MOFun's Ullmann bijection finder
 
-if abspath(PROGRAM_FILE) == @__FILE__
+function ullmann_demo()
     # Build parent Crystal
     Co2m_dobdc = Crystal("KOSKIO_clean.cif")
     infer_bonds!(Co2m_dobdc, true)
@@ -12,10 +13,16 @@ if abspath(PROGRAM_FILE) == @__FILE__
 
     # Search for search Moiety in parent Crystal
     matches = ullmann_bijections(m_dobdc, Co2m_dobdc)
+## TODO refactor as xyz_find
 
     # Replace with new Moiety
     f2_m_dobdc = moiety("data/fragments/F2-m-dobdc.xyz")
-    ## TODO
+## TODO implement xyz_find_replace
+end
+
+if (abspath(PROGRAM_FILE) == @__FILE__) || ((@isdefined Atom) && typeof(Atom) == Module)
+
+    ullmann_demo()
 
     exit(0)
 end
