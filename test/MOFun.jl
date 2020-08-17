118c0fb02eb76bab439e893800539ffe2ed72e4b
eahenle <eahenle@gmail.com>
2020-08-17 02:34:42 -0700

    Update MOFun.jl

--- test/MOFun.jl
+++ test/MOFun.jl
@@ -1,6 +1,7 @@
 module MOFun_Test
 
-using MOFun, PorousMaterials, Test
+using Test
+using PorousMaterials, MOFun, Moiety
 
 irmof1 = Crystal("IRMOF-1.cif")
 strip_numbers_from_atom_labels!(irmof1)
@@ -12,9 +13,11 @@ timil125 = Crystal("Ti-MIL-125.cif")
 strip_numbers_from_atom_labels!(timil125)
 infer_bonds!(timil125, true)
 moty = moiety("p-phenylene")
+moty_w_R_grp = moiety("2-R-p-phenylene")
 
 @testset "MOFun Tests" begin
-    @test length(substructure_search(moty, irmof1)) == 96
-    @test length(substructure_search(moty, timil125)) == 48
+    @test length(moty ∈ irmof1) == 24
+    @test length(moty ∈ timil125) == 12
+    @test length(moty_w_R_grp ∈ timil125) == 12
 end
 end
