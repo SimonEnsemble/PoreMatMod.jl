9d653959da09432e58d4924704e52a11a50dbd2a
eahenle <eahenle@gmail.com>
2020-12-10 12:44:12 -0800

    Create landingpage.jl

---
+++ docs/src/examples/landingpage.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6e9609e0-32dd-11eb-2ae9-5b71bd231df6
+
+
+# ╔═╡ Cell order:
+# ╠═6e9609e0-32dd-11eb-2ae9-5b71bd231df6
