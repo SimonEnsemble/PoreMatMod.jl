99d32eb0bec983b2b3595cc0986b32c59ee9aff9
eahenle <eahenle@gmail.com>
2022-02-16 21:53:51 -0800

    aqua

---
+++ test/aqua.jl
@@ -0,0 +1,14 @@
+using PoreMatMod
+import Aqua
+
+# ambiguity testing finds many "problems" outside the scope of this package
+ambiguities=false
+
+# to skip when checking for stale dependencies and missing compat entries
+#   Aqua is added in a separate CI job, so (ironically) does not work w/ itself
+stale_deps = (ignore=[:Aqua],)
+
+Aqua.test_all(PoreMatMod;
+    ambiguities=ambiguities, 
+    stale_deps=stale_deps
+)
