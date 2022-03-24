6d99ad73f499b11653b5e41f48711985bf0087dc
Adrian Henle <eahenle@gmail.com>
2022-03-23 18:12:49 -0700

    download fallback (#126)

--- src/misc.jl
+++ src/misc.jl
@@ -4,10 +4,16 @@
 Launches the GUI Pluto notebook.
 """
 function PoreMatModGO()
-    # notebook path
-    pmmg_ntbk = joinpath(pathof(PoreMatMod), "..", "PoreMatModGO.jl")
-    # run the notebook in Pluto
-    Pluto.run(notebook=pmmg_ntbk)
+    try
+        # notebook path
+        pmmg_ntbk = joinpath(pathof(PoreMatMod), "..", "PoreMatModGO.jl")
+        # run the notebook in Pluto
+        Pluto.run(notebook=pmmg_ntbk)
+    catch
+        # download as a temporary file in case of access issues
+        pmmg_ntbk = download("https://raw.githubusercontent.com/SimonEnsemble/PoreMatMod.jl/master/src/PoreMatModGO.jl")
+        Pluto.run(notebook=pmmg_ntbk)
+    end
 end
 
 
